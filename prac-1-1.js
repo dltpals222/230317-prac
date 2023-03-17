3f4822c33244cba873d53bed1fffa2f245d56278
leesemin <leesemin222@gmail.com>
2023-03-17 15:35:39 +0900

    prac-1.js의 결과가 마구잡이로 나온다길래 해봄(결과 : 순서대로 나옴)

---
+++ prac-1-1.js
@@ -0,0 +1,44 @@
+setTimeout(function() {
+  console.log("첫번째 함수");
+},1000) // 첫번째 함수 setTimeout 끝
+
+setTimeout(function() {
+  console.log("두번째 함수");
+},1000) // 두번째 함수 setTimeout 끝
+
+setTimeout(function(){
+  console.log("세번째 함수");
+},1000) //세번째 함수 setTimeout 끝
+
+setTimeout(function (){
+  console.log("네번째 함수");
+},1000) // 네번째 함수 setTimeout 끝
+
+setTimeout(function (){
+  console.log("다섯번째 함수");
+},1000) // 다섯번째 함수 setTimeout 끝
+
+setTimeout(function() {
+  console.log("여섯번째 함수");
+},1000) // 여섯번째 함수 setTimeout 끝
+
+setTimeout(function() {
+  console.log("일곱번째 함수");
+},1000) // 일곱번째 함수 setTimeout 끝
+
+setTimeout(function(){
+  console.log("여덟번째 함수");
+},1000) //여덟번째 함수 setTimeout 끝
+
+setTimeout(function (){
+  console.log("아홉번째 함수");
+},1000) // 아홉번째 함수 setTimeout 끝
+
+setTimeout(function (){
+  console.log("열번째 함수");
+},1000) // 열번째 함수 setTimeout 끝
+
+
+// setTimeout(function() {console.log("첫번째 함수");setTimeout(function() {console.log("두번째 함수"); setTimeout(function(){ console.log("세번째 함수");setTimeout(function (){console.log("네번째 함수");},1000)},1000)},1000)},1000)
+
+// setTimeout(function() {console.log("첫번째 함수");},1000);setTimeout(function() {console.log("두번째 함수");},1000);setTimeout(function(){console.log("세번째 함수");},1000);setTimeout(function (){console.log("네번째 함수");},1000);setTimeout(function (){console.log("다섯번째 함수");},1000);setTimeout(function() {console.log("여섯번째 함수");},1000);setTimeout(function() {console.log("일곱번째 함수");},1000);setTimeout(function(){console.log("여덟번째 함수");},1000);setTimeout(function (){console.log("아홉번째 함수");},1000);setTimeout(function (){console.log("열번째 함수");},1000);
